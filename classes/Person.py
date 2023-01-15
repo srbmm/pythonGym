fd03d7d971173d498a282294a095eb6b7414c10a
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-01-15 17:32:00 +0330

    Person&Athlete class complete

--- classes/Person.py
+++ classes/Person.py
@@ -1,9 +1,19 @@
 class Person:
-    def __init__(self, name, sex, age, weight):
+    def __init__(self, name: str, sex: str, age: float, weight: float):
         self.name = name
         self.sex = sex
         self.age = age
         self.weight = weight
 
-def Athlete(Person):
-    def __init__(self,)
+    def __str__(self):
+        return self.name
+
+
+class Athlete(Person):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.program = Program(self)
+
+
+test = Athlete("ali", "m", 17, 60, "test")
+test.print()
